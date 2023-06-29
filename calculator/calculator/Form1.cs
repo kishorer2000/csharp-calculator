0f2c575c88dcf6770d35921cbb63ab7be8e652d6
Kishore R <kishore2000.kr@gmail.com>
2023-06-29 15:17:04 +0530

    Add files via upload

---
+++ calculator/calculator/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class calculator : Form
+    {
+        public decimal a;
+        public decimal b;
+        public decimal c;
+        public string operators;
+        public calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="0")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text += "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += "4";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += "5";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            a = decimal.Parse(textBox1.Text);
+            textBox1.Clear();
+            operators = "+";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            a = decimal.Parse(textBox1.Text);
+            textBox1.Clear();
+            operators = "-";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            a = decimal.Parse(textBox1.Text);
+            textBox1.Clear();
+            operators = "*";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            a = decimal.Parse(textBox1.Text);
+            textBox1.Clear();
+            operators = "/";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            switch (operators)
+            {
+                case "+":
+                    b = decimal.Parse(textBox1.Text);
+                    c = a + b;
+                    textBox1.Text = c.ToString();
+                    break;
+                case "-":
+                    b = decimal.Parse(textBox1.Text);
+                    c = a - b;
+                    textBox1.Text = c.ToString();
+                    break;
+                case "*":
+                    b = decimal.Parse(textBox1.Text);
+                    c = a * b;
+                    textBox1.Text = c.ToString();
+                    break;
+                case "/":
+                    b = decimal.Parse(textBox1.Text);
+                    c = a / b;
+                    textBox1.Text = c.ToString();
+                    break;
+            }
+        }
+    }
+}
